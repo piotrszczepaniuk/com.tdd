e7bf3195e15b8b9ecfa2081e57136a7638b89637
PISZC <piszc@orsted.dk>
2020-01-15 13:50:07 +0100

    Disable tests, which are failling

--- src/test/java/bookstoread/FailureAndErrorTests.java
+++ src/test/java/bookstoread/FailureAndErrorTests.java
@@ -1,10 +1,13 @@
 package bookstoread;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
+@Disabled("This tests are failling for reason")
 public class FailureAndErrorTests {
 
     @Test
